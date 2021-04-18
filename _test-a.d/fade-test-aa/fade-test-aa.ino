b3961aad08602571f9d2706f1b31bd1f3572b93f
wa1tnr <wa1tnr@users.noreply.github.com>
2021-04-18 21:59:38 +0000

    formatting

--- _test-a.d/fade-test-aa/fade-test-aa.ino
+++ _test-a.d/fade-test-aa/fade-test-aa.ino
@@ -1,18 +1,20 @@
 /*
-  Fade
+    Fade
 
-  This example shows how to fade the onboard Raspberry Pi Pico LED
+    This example shows how to fade the onboard Raspberry Pi Pico LED
 
-  This example code is in the public domain.
+    This example code is in the public domain.
 
-  http://www.arduino.cc/en/Tutorial/Fade
+    http://www.arduino.cc/en/Tutorial/Fade
 */
 
 /*
 
-   18 April 2021 - following the consequences of:
+    18 April 2021 - following the consequences of:
 
-   [ https://github.com/earlephilhower/arduino-pico#installing-via-git ]
+    [ https://github.com/earlephilhower/arduino-pico#installing-via-git ]
+
+    Sun Apr 18 21:58:58 UTC 2021
 
 */
 
@@ -22,22 +24,22 @@ int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // declare pin to be an output:
-  pinMode(led, OUTPUT);
+    // declare pin to be an output:
+    pinMode(led, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness
-  analogWrite(led, brightness);
+    // set the brightness
+    analogWrite(led, brightness);
 
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
+    // change the brightness for next time through the loop:
+    brightness = brightness + fadeAmount;
 
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
-  }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+    // reverse the direction of the fading at the ends of the fade:
+    if (brightness <= 0 || brightness >= 255) {
+        fadeAmount = -fadeAmount;
+    }
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
 }
