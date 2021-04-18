dde12b755caae9a7cf0f2a9244350233698f0d82
wa1tnr <wa1tnr@users.noreply.github.com>
2021-04-18 22:40:20 +0000

    Reflash automatically after 12 seconds
    Instantiates the USB drive on your host PC,
    so that a new version of the .UF2 can be
    dragged and dropped.
    
    Uses Gammon's idea for an empty .ino sketch,
    with the real sketch in a companion .cpp file.
    
    Used 'ag' to find the right code base, which
    seemed to be:
    
     $ ag usb_boot | cat -n
         1  cores/rp2040/SerialUSB.cpp:294:        reset_usb_boot(0,0);
    
    Then just swiped all the stuff from SerialUSB.cpp and put
    it into the sketch, without examination of any sort.
    
    'Just worked'.  Probably quite 'wrong' of course.  Quick test
    of the concept.
    
    	modified:   _test-a.d/fade-test-ac/fade-test-ac.ino
    	new file:   _test-a.d/fade-test-ac/fade-test-ac_SKETCH.cpp
    
    On branch dvlp-aa

---
+++ _test-a.d/fade-test-ac/fade-test-ac_SKETCH.cpp
@@ -0,0 +1,66 @@
+/*
+    Fade
+
+    This example shows how to fade the onboard Raspberry Pi Pico LED
+
+    This example code is in the public domain.
+
+    http://www.arduino.cc/en/Tutorial/Fade
+*/
+
+/*
+
+    18 April 2021 - following the consequences of:
+
+    [ https://github.com/earlephilhower/arduino-pico#installing-via-git ]
+
+    Sun Apr 18 21:58:58 UTC 2021
+
+*/
+#include <Arduino.h>
+#include "CoreMutex.h"
+
+#include "tusb.h"
+#include "pico/time.h"
+#include "pico/binary_info.h"
+#include "pico/bootrom.h"
+#include "hardware/irq.h"
+#include "pico/mutex.h"
+#include "hardware/watchdog.h"
+#include "pico/unique_id.h"
+
+int led = LED_BUILTIN; // the PWM pin the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+// the setup routine runs once when you press reset:
+void setup() {
+    // declare pin to be an output:
+    pinMode(led, OUTPUT);
+    Serial.begin(9600);
+    while (!Serial);
+    delay(5500);
+    Serial.println("Hello from Arduino side RP2040 foo!");
+    Serial.println("Reflash in 12 seconds.");
+    delay(12000);
+
+    // reflash
+    // #include pico/bootrom.h
+    reset_usb_boot(0, 0);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+    // set the brightness
+    analogWrite(led, brightness);
+
+    // change the brightness for next time through the loop:
+    brightness = brightness + fadeAmount;
+
+    // reverse the direction of the fading at the ends of the fade:
+    if (brightness <= 0 || brightness >= 255) {
+        fadeAmount = -fadeAmount;
+    }
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+}
