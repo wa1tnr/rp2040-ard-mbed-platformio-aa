8409bafcc5f10cdbdf6c19615f869ca0792d840a
wa1tnr <wa1tnr@users.noreply.github.com>
2021-04-19 21:33:42 +0000

    monitor - mainfile - sketch

---
+++ monitor.d/monitor-aa/n_monitor-aa_SKETCH.cpp
@@ -0,0 +1,122 @@
+// n_monitor-aa_SKETCH.cpp
+// Mon Apr 19 21:29:48 UTC 2021
+
+// was: forth-aa_SKETCH.cpp
+// was: ITC-Forth.ino
+
+/*
+   from: https://github.com/CharleyShattuck/Steno-Keyboard-Arduino.git
+*/
+
+#include <Arduino.h>
+#define REVISION_ITCF "0.1.0-c.0"
+#define SLOW_WAIT_AA 125
+
+#define RAM_SIZE 0x1200
+#define S0 0x1000
+#define R0 0x0f00
+
+extern void reflash_firmware(void); // prototype
+
+// hardwara GPIO
+int led = LED_BUILTIN;
+
+// "registers"
+int S = S0; // data stack pointer
+int R = R0; // return stack pointer
+int I = 0; // instruction pointer
+int W = 0; // working register
+
+int reflash_timeout = 7; // seconds
+
+const int memory [] {
+  1, // print A
+  2, // delay 1 sec
+  3, // branch
+  0, // to this address
+};
+
+void runForth () {
+next:
+  W = memory [I++];
+  switch (W) {
+    case 1:
+    A:
+      reflash_timeout--;
+      Serial.write ('A');
+      goto next;
+    case 2:
+    _delay:
+      delay (1000);
+      goto next;
+    case 3:
+    branch:
+      I = memory [I];
+      if (reflash_timeout == 0) return;
+      goto next;
+  }
+}
+
+void full_blank(void) {
+  digitalWrite(led, 0);
+}
+
+void blink (void) {
+  digitalWrite(led, 1);
+  delay(55);
+  full_blank();
+}
+
+void fast_blank(void) {
+  int counter = SLOW_WAIT_AA;
+  do {
+    if (Serial) return;
+    delay(50);
+    counter--;
+  } while (counter > 0);
+  full_blank();
+}
+
+void await_serial(void) {
+  while(!Serial) {
+    blink ();
+    if(!Serial) fast_blank();
+    full_blank();
+  }
+  Serial.print("Rev. ");
+  Serial.println(REVISION_ITCF);
+}
+
+void pre_serial(void) {
+  while(!Serial) {
+    await_serial();
+  }
+}
+
+void hardware_setup(void) {
+  pinMode(led, OUTPUT);
+}
+
+void reflash(void) {
+  reflash_firmware();
+}
+
+void setup () {
+  Serial.begin (9600);
+  hardware_setup();
+  pre_serial();
+  I = 0;
+  S = S0;
+  R = R0;
+  runForth (); // loop; not easily escaped
+  reflash();
+}
+
+void loop () {
+  while (1) {
+//    Serial.write ('A');
+//    delay (1000);
+  };
+}
+
+// End
