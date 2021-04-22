eba2ae3c821d7cf9c27ad84f20305f0e50741857
wa1tnr <wa1tnr@users.noreply.github.com>
2021-04-22 23:42:26 +0000

    New subdir same branch
    	new file:   dump_ram.cpp
    	new file:   monitor-ee.ino
    	new file:   reflash.cpp
    
    On branch dvlp-aa-dump-a-

---
+++ monitor.d/monitor-ee/dump_ram.cpp
@@ -0,0 +1,51 @@
+/* dump_ram.cpp */
+#include <Arduino.h>
+#define REVISION_ITCF "0.1.0-g.6g - alpha kiyuta ii"
+// Thu Apr 22 23:32:07 UTC 2021
+
+// older:
+// #define REVISION_ITCF "0.1.0-g.6 - alpha kiyuta ii"
+// Thu Apr 22 18:02:54 UTC 2021
+
+/*
+  from: https://github.com/wa1tnr/Metro-M4-Express-interpreter
+  from: https://github.com/CharleyShattuck/Feather-M0-interpreter
+*/
+
+extern void push(int n);
+extern int pop(void);
+
+/* dump 16 bytes of RAM in hex with ascii on the side */
+
+void dumpRAM(void) {
+  char buffer[5] = "";
+  char *ram;
+  int p = pop();
+  ram = (char*)p;
+  sprintf(buffer, "%4x", p);
+  Serial.print(buffer);
+  Serial.print("   ");
+  for (int i = 0; i < 16; i++) {
+    char c = *ram++;
+    sprintf(buffer, " %2x", (c & 0xff));
+    Serial.print(buffer);
+  }
+  ram = (char*)p;
+  Serial.print("   ");
+  for (int i = 0; i < 16; i++) {
+    buffer[0] = *ram++;
+    if (buffer[0] > 0x7f || buffer[0] < ' ') buffer[0] = '.';
+    buffer[1] = '\0';
+    Serial.print(buffer);
+  }
+  push(p + 16);
+}
+
+/* dump 256 bytes of RAM */
+void rdumps(void) {
+  for (int i = 0; i < 16; i++) {
+    Serial.println();
+    dumpRAM();
+  }
+}
+
