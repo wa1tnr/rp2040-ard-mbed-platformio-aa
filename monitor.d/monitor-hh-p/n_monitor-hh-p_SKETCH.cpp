ce0e74001edec3c17b9c2cc5fad42af2fb922f25
wa1tnr <wa1tnr@users.noreply.github.com>
2021-04-25 19:26:05 +0000

    SECONDARY interpreter - any keystroke launches it
    Seems to launch on its own (no penalty though)
    
    Which would mean it's seeing a keystroke when
    the serial port first connects, possibly.
    
    	new file:   README.md
    	new file:   dump_ram.cpp
    	new file:   monitor-hh-p.ino
    	new file:   n_monitor-hh-p_SKETCH.cpp
    	new file:   reflash.cpp
    	new file:   second_interp.cpp
    
    On branch dvlp-interp-exp-a

---
+++ monitor.d/monitor-hh-p/n_monitor-hh-p_SKETCH.cpp
@@ -0,0 +1,631 @@
+// n_monitor-gg_SKETCH.cpp
+#include <Arduino.h>
+#define REVISION_ITCF "0.1.0-h.0a - alpha mivurnix i np: cc"
+
+// TODO: add command interpreter as a sub-process, made
+// active by a simpe keystroke (use getch model explored
+// earlier).
+
+// DONE: TODO: make program boundary accurate or meaningful
+// op_rba active now.
+
+// Sun Apr 25 16:07:02 UTC 2021
+
+// was: n_monitor-gg_SKETCH.cpp
+// was: forth-aa_SKETCH.cpp
+// was: ITC-Forth.ino
+
+/*
+    from: https://github.com/CharleyShattuck/Steno-Keyboard-Arduino.git
+*/
+
+/*
+  stack
+  from: https://github.com/CharleyShattuck/Feather-M0-interpreter
+*/
+
+/*
+   dump
+   from: https://github.com/wa1tnr/Metro-M4-Express-interpreter/blob/master/interpret_m4/interpret_m4.ino
+*/
+
+#undef ADAFRUIT_ITSY_RP2040_ITCF
+#define ADAFRUIT_ITSY_RP2040_ITCF
+
+#ifndef ADAFRUIT_ITSY_RP2040_ITCF
+    #define UNKNOWN_TARGET_RP2040_ITCF
+#endif
+
+#define SLOW_WAIT_AA 125
+
+// #define RAM_SIZE 0x1200
+
+#define RAM_SIZE 0x2C8
+
+// program_boundary is set arbitrarily as a 'fence'.
+// code found above the fence will throw an exception.
+// it is an address bounds checker/enforcer.
+// can be set very close to the end of the RAMSPACE array.
+
+// much shorter than 180 will set off an exception, in
+// the current program (25 April 2021 02:36 UTC).
+
+#define program_boundary 180 // 0xB4
+
+// #define RAM_SIZE 0x500
+#define S0 0x1000
+#define R0 0x0f00
+
+int RAMSPACE [RAM_SIZE];
+
+/* Data stack for parameter passing
+   This "stack" is circular,
+   like a Green Arrays F18A data stack,
+   so overflow and underflow are not possible
+   Number of items must be a power of 2 */
+
+const int STKSIZE = 8;
+const int STKMASK = 7;
+int stack[STKSIZE];
+int p = 0;
+int reflash_timeout = 0xCFFF; // a good six minutes here 0xCFFF
+int mem_size = 0; // for sizeof<foo>
+
+/* TOS is Top Of Stack */
+#define TOS stack[p]
+
+
+// hardwara GPIO
+#ifdef UNKNOWN_TARGET_RP2040_ITCF
+  #undef ADAFRUIT_ITSY_RP2040_ITCF
+int led = LED_BUILTIN;
+#endif
+
+#ifdef ADAFRUIT_ITSY_RP2040_ITCF
+int led = 11;
+#endif
+
+// "registers"
+int S = S0; // data stack pointer
+int R = R0; // return stack pointer
+int I = 0; // instruction pointer
+int W = 0; // working register
+
+#define _nop_hxpfx   0x111130
+#define _nop_hxlg    0x03000000
+
+#define op_exc       0xced1
+#define op_ext       0x747865 // exception testing (deliberate)
+#define op_lit       0x74696C + _nop_hxlg // lit: 6C l  69 i  74 t  0x74696C
+#define op_nop       0x706F6E + _nop_hxlg // n: 6e o: 6f p: 70
+#define op_n0p       0x4F4F4F4F
+#define op_rba       0x616272 + _nop_hxlg
+#define op_rmb       0x626D72 + _nop_hxlg // hex char b . char m . char r . 62 6D 72  ok
+
+#define op_rfl       0x6C6672 + _nop_hxlg
+#define op_dly       0x796C64 + _nop_hxlg
+// #define op_getch 3
+#define op_getch     0x79656B + _nop_hxlg // 'key' char y . char e . char k . 79 65 6B  ok
+
+#define op_push 4
+#define op_pop 5
+// #define op_tbz 6
+#define op_tbz       0x7A6274 + _nop_hxlg // char z . char b . char t . 7A 62 74  ok
+
+#define op_branch 7
+#define op_one_plus 9
+#define op_dts       0x737464 + _nop_hxlg
+#define op_pon       0x6E6F70 + _nop_hxlg
+#define op_pof       0x666F70 + _nop_hxlg
+#define op_dump      0x706D64 + _nop_hxlg
+#define op_emit      0x746D65 + _nop_hxlg
+#define op_romptr 16
+
+// #define op_hxee 238
+// #define op_hxee 0x3EEEEEE // 238
+// char e . char e . char x . char h . 65 65 78 68  ok
+// #define op_hxee 0x657868EE
+#define op_hxee 0x646E45EE
+
+#define n1_sec  999
+#define n4_sec 3999
+#define n8_sec 7999
+#define c_newline 10
+#define c_return 13
+
+const int memory [] {
+
+     op_nop, op_nop, op_nop, op_nop,
+     op_nop, op_nop, op_nop, op_nop,
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     /* pick one: op_rba  ..  or  ..  op_rmb  .. here: */
+     // op_rba,
+     // op_rmb,
+     op_rba,
+     op_dts,
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dump,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dump,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dump,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dump,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dump,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dump,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dump,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dump,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_dts,
+
+     /* dump */
+
+     /* dump */
+
+     /* dump */
+
+     /* dump */
+
+     /* dump */
+
+     /* dump */
+
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+
+     op_lit, 4000, op_dly,
+
+
+     /* blink */
+     op_lit, led, op_pon,  op_lit,  1100, // BLINK DELAY late in program exec.
+     op_dly,
+     op_lit, led, op_pof, op_lit, n1_sec, op_dly,
+
+     op_lit, n1_sec, op_dly,
+
+     op_nop,
+     op_lit, c_newline, op_lit, c_return, op_emit, op_emit,
+     op_lit, 4733, op_dly, // 4733 is 0x127D
+
+//
+// 2000246C: 6C 69 74  3  7D 12 00 00  64 6C 79  3  72 66 6C  3  lit.}...dly.rfl.
+//                        ^^ ^^
+
+     // ------------------------------
+     // ------------------------------
+     // ------------------------------
+     // op_ext, // throw a test exception
+     // ------------------------------
+     // ------------------------------
+     // ------------------------------
+     op_getch,
+     op_rfl,
+
+     op_lit, 0, op_lit, 7, op_lit, 14, op_lit, 21, op_lit, 28, op_lit, 35, op_lit, 42, op_lit, 49,
+     op_dts,
+
+     op_lit, n8_sec, op_dly,
+
+     op_lit, 5, op_lit, 10, op_lit, 15, op_lit, 20, op_lit, 25, op_lit, 30, op_lit, 35, op_lit, 40,
+
+     op_dts,
+
+     op_lit, n8_sec, op_dly,
+
+     op_lit, 0, op_lit, 7, op_lit, 14, op_lit, 21, op_lit, 28, op_lit, 35, op_lit, 42, op_lit, 49,
+     op_dts,
+
+     op_lit, n8_sec, op_dly,
+
+     op_lit,  3, op_lit,  6, op_lit,  9, op_lit, 12, op_lit, 15, op_lit, 18, op_lit, 21, op_lit, 24,
+
+     op_dts,
+     op_lit, n8_sec, op_dly,
+
+     op_dts,
+     op_lit, n4_sec, op_dly,
+     op_dts,
+     op_lit, n4_sec, op_dly,
+     op_dts,
+     op_lit, n4_sec, op_dly,
+     op_dts,
+     op_lit, n4_sec, op_dly,
+     op_nop,
+
+//  LINE 472 is 98
+     op_dts, //
+
+     op_tbz,
+
+
+     op_dts,
+
+     op_rfl,
+
+// try 103 at Line 472
+     op_nop,
+     op_nop, op_nop, op_nop, op_nop,
+     op_nop, op_nop, op_nop, op_nop,
+     op_nop, op_nop, op_nop, op_nop,
+     op_nop, op_nop, op_nop, op_nop,
+     op_hxee, op_hxee, op_hxee, op_hxee,
+     op_hxee, op_hxee, op_hxee, op_hxee,
+     op_hxee, op_hxee, op_hxee, op_hxee,
+     op_hxee, op_hxee, op_hxee, op_hxee,
+     op_hxee, op_hxee, op_hxee, op_hxee,
+     op_hxee, op_hxee, op_hxee, op_hxee,
+     op_hxee, op_hxee, op_hxee, op_hxee,
+     op_hxee, op_hxee, op_hxee, op_hxee, // compiler lets you keep your comma
+
+     };
+
+// uint32_t mem_rom = *memory;
+uint32_t mem_rom = (uint32_t) &memory;
+uint32_t mem_ram = (uint32_t) &RAMSPACE;
+
+// https://github.com/CharleyShattuck/Feather-M0-interpreter/blob/master/Interpreter.ino
+
+/* Terminal Input Buffer for interpreter */
+const byte maxtib = 16;
+char tib[maxtib];
+byte pos = 0;
+
+int faked = 0;
+int L = 0; // place to pop into
+
+extern void setup_second_interpreter(void);
+extern void secondary_Forth_loop(void);
+
+extern void reflash_firmware(void); // prototype
+extern void dumpRAM(void); // dump_ram.cpp
+extern void rdumps(void);
+
+// #define FILL_CHAR 514
+#define FILL_CHAR 0x4F4F4F4F
+#define xxFILL_CHAR 0x5DDDDDDD
+// #define FILL_CHAR 1
+/* copy ROM-like space into RAM-like space: */
+void copy_over(void) {
+    int mem_size = sizeof(memory) / 4;
+
+    // fill ram space with nonsense, first.
+    for (int i=0; i< (RAM_SIZE); i++) {
+        RAMSPACE[i] = FILL_CHAR;
+    }
+    for (int i= (RAM_SIZE - 256); i< RAM_SIZE; i++) {
+        RAMSPACE[i] = op_nop; // nop
+    }
+
+    /* sneak one in, x4 */
+    int i = RAM_SIZE - 77; RAMSPACE[i] = 0x2222; // garbage
+    i = RAM_SIZE - 78; RAMSPACE[i] = 0x2222;
+    i = RAM_SIZE - 79; RAMSPACE[i] = 0x2222;
+    i = RAM_SIZE - 80; RAMSPACE[i] = 0x2222;
+
+    for (int i= (RAM_SIZE - 64); i< RAM_SIZE; i++) { // top end
+        RAMSPACE[i] = op_exc; // throw exception
+    }
+    // copy active program into ram, for execution in switch/case
+    for (int i=0; i < mem_size; i++) {
+        RAMSPACE[i] = memory [i];
+    }
+
+}
+
+/* push n to top of data stack */
+void push(int n) {
+  p = (p + 1)& STKMASK;
+  TOS = n;
+}
+
+/* return top of stack */
+int pop(void) {
+  int n = TOS;
+  p = (p - 1)& STKMASK;
+  return n;
+}
+
+void reflash(void) {
+    reflash_firmware();
+}
+
+void await_serial(void) {
+}
+
+void pre_serial(void) {
+    delay(700);
+    pinMode(led, OUTPUT);
+    digitalWrite(led, 1);
+    delay(700);
+    digitalWrite(led, 0);
+    delay(700);
+    while (!Serial) {
+        await_serial();
+    }
+    Serial.println(REVISION_ITCF);
+    delay(3000);
+}
+
+void nopp(void) { } // no operation
+
+void print_newline(void) {
+    Serial.write('\r');
+    Serial.write('\n');
+}
+
+void print_stack_report(void) {
+    Serial.println();
+    // Serial.println(" <4> 1 2 3 4 :: Reads left to right, old to new: ");
+    for (int k = 0; k < (STKSIZE + 1); k++) L = pop();
+    for (int i = 0; i < STKSIZE; i++) {
+        for (int j = 0; j < ((STKSIZE - 1)) ; j++) {
+            L = pop();
+        }
+        Serial.print(L);
+        Serial.print(' ');
+    }
+    for (int l = 0; l < (STKSIZE - 1); l++) L = pop();
+    Serial.println();
+    // delay(4000);
+}
+
+void handle_exception(void) {
+    Serial.println(" EXCEPTION encountered. delay 2 seconds..");
+    delay(2000);
+    Serial.println("reflash(); called now.");
+    reflash();
+}
+
+bool extra_printing = 0; // -1;
+
+void runForth () {
+    char ch;
+    int ESC_counter = 0;
+    bool return_Flag = 0;
+next:
+    if (I < 0) I = program_boundary + 8;
+    if (I > program_boundary) I = program_boundary + 8;
+
+    if (extra_printing) {
+        Serial.write(' '); Serial.write(' '); Serial.write('[');
+        Serial.print(I);
+        Serial.write(']'); Serial.write(' '); Serial.write(' ');
+        delay(200);
+    }
+
+    if (I > program_boundary) handle_exception();
+
+    // W = memory [I++];
+    W = RAMSPACE [I++];
+    switch (W) {
+        case op_exc:
+        _exc:
+            Serial.println(""); Serial.print("  * * *   EXCEPTION   * * *  ");
+            I = I + program_boundary + 8;
+            goto next;
+
+        case op_ext:
+        _ext:
+            Serial.println(""); Serial.print("  * * *  test exception   * * *  ");
+            // I = I + 192;
+            // I = I + 160;
+            I = I +  106; // 103
+            goto next;
+
+        case op_n0p:
+            Serial.print("~~~op_n0p~~");
+        case op_nop:
+        _nop:
+            Serial.print(" op_nop");
+            nopp();
+            goto next;
+
+        case op_dly:
+        _delay:
+            Serial.print(" op_dly");
+            L = pop();
+            delay(L);
+            goto next;
+
+        case op_getch:
+        _getch:
+            Serial.print(" op_getch");
+            ch = '\000';
+            if (Serial.available() > 0) ch = Serial.read();
+            // Serial.println("TADA!"); delay(5000);
+            Serial.println();
+            Serial.println("entering Forth-like Interpreter - secondary"); delay(1000);
+            Serial.println();
+            Serial.println("type: 'words' for vocabulary list.");
+            secondary_Forth_loop();
+            Serial.println();
+            Serial.println("EXITED secondary interpreter.");
+            Serial.println();
+            delay(2000);
+            goto next;
+
+        case op_push: // forbidden idiom - maybe - use LIT instead
+        _push:
+            Serial.print(" op_push");
+            push(++faked);
+            goto next;
+
+        case op_pop:
+        _pop:
+            Serial.print(" op_pop");
+            L = pop();
+            goto next;
+
+        case op_tbz:
+        _tib_init:
+            Serial.print(" op_tbz");
+            pos = 0; tib[0] = 0;
+            goto next;
+
+        case op_branch:
+        _branch:
+            Serial.print(" op_branch");
+            I = memory [I];
+            goto next;
+
+        case op_rfl:
+        _reflash:
+            Serial.print(" op_rfl");
+            Serial.print(" delay 2 seconds..");
+            delay(2000);
+            reflash();
+            goto next;
+
+        case op_one_plus:
+        _one_plus:
+            Serial.print(" op_one_plus");
+            L = pop();
+            L++;
+            push(L);
+            goto next;
+
+        // case op_stack_report:
+        case op_dts:
+        _stack_report:
+            print_stack_report();
+            goto next;
+
+        case op_lit:
+        _lit:
+            push(memory[I]);
+            I++;
+            goto next;
+
+        // case op_gpio_on:
+        case op_pon:
+        _gpio_on:
+//          Serial.print(" op_gpio_on");
+            Serial.print(" op_pon");
+            L = pop();
+            digitalWrite(L, 1);
+            goto next;
+
+        // case op_gpio_off:
+        case op_pof:
+        _gpio_off:
+        //  Serial.print(" op_gpio_off");
+            Serial.print(" op_pof");
+            L = pop();
+            digitalWrite(L, 0);
+            goto next;
+
+        case op_dump:
+        _dump:
+            Serial.print(" op_dump");
+            // Serial.println("");
+            // Serial.println("    OOB: 0x12B8 0x17D0 ROM");
+            // Serial.println("    OOB: 0x21BC 0x26D3 RAM");
+
+    // 200021BC: 6E 6F 70  3  6E 6F 70  3  6E 6F 70  3  6E 6F 70  3  nop.nop.nop.nop.
+
+
+    // 200026CC: EE 45 6E 64  EE 45 6E 64  4F 4F 4F 4F  4F 4F 4F 4F  .End.EndOOOOOOOO
+
+            rdumps();
+            goto next;
+
+        case op_emit:
+        _emit:
+            // Serial.print(" op_emit");
+            Serial.write((pop()));
+            goto next;
+
+        case op_rba: // 'ram base' e.g. 'rba' ram base
+        _ramptr:
+            Serial.print(" op_rba");
+            push(mem_ram);
+            goto next;
+
+        case op_rmb: // 'rom base' 'rmb' rom base
+        _romptr:
+            Serial.print(" op_rmb");
+            push(mem_rom);
+            goto next;
+
+/*
+        case op_hxee:
+        _hxee:
+            // nothing - is a marker instruction only
+            goto next;
+*/
+        default:
+            Serial.println("");
+            Serial.println("");
+            Serial.println("   * * *   NO SUCH INSTRUCTION   * * *   ");
+            Serial.println("");
+            Serial.println("");
+            goto next;
+    }
+}
+
+void setup () {
+    Serial.begin (9600);
+    pre_serial();
+    setup_second_interpreter();
+    I = 0;
+    S = S0;
+    R = R0;
+    copy_over(); // where?
+    for (int i = 0; i < STKSIZE; i++) push((13 * i));
+    runForth (); // loop; not easily escaped
+}
+
+void loop () {
+    while (1) {
+        //    Serial.write ('A');
+        //    delay (1000);
+    };
+}
+
+// End
---
+++ monitor.d/monitor-hh-p/second_interp.cpp
@@ -0,0 +1,448 @@
+#include <Arduino.h>
+/* Tiny interpreter,
+   similar to myforth's Standalone Interpreter
+   This example code is in the public domain */
+
+/* Structure of a dictionary entry */
+typedef struct {
+  const char *name;
+  void (*function)();
+} entry;
+
+/* Data stack for parameter passing
+   This "stack" is circular,
+   like a Green Arrays F18A data stack,
+   so overflow and underflow are not possible
+   Number of items must be a power of 2 */
+const int STKSIZE_B = 8;
+const int STKMASK_B = 7;
+int stack_B[STKSIZE_B];
+int p_B = 0;
+
+/* TOS is Top Of Stack */
+#define TOS stack_B[p_B]
+/* NAMED creates a string in flash */
+#define NAMED(x, y) const char x[]=y
+
+/* Terminal Input Buffer for interpreter */
+const byte maxtib_B = 16;
+char tib_B[maxtib_B];
+/* buffer required for strings read from flash */
+char namebuf[maxtib_B];
+byte pos_B;
+
+/* push n to top of data stack */
+void push_B(int n) {
+  p_B = (p_B + 1)& STKMASK_B;
+  TOS = n;
+}
+
+/* return top of stack */
+int pop_B() {
+  int n = TOS;
+  p_B = (p_B - 1)& STKMASK_B;
+  return n;
+}
+
+/* Global delay timer */
+int spd = 15;
+
+/* top of stack becomes current spd */
+NAMED(_speed, "speed");
+void speed() {
+  spd = pop_B();
+}
+
+/* discard top of stack */
+NAMED(_drop, "drop");
+void drop() {
+  pop_B();
+}
+
+/* recover dropped stack item */
+NAMED(_back, "back");
+void back() {
+  for (int i = 1; i < STKSIZE_B; i++) drop();
+}
+
+/* copy top of stack */
+NAMED(_dup, "dup");
+void dup() {
+  push_B(TOS);
+}
+
+/* exchange top two stack items */
+NAMED(_swap, "swap");
+void swap() {
+  int a;
+  int b;
+  a = pop_B();
+  b = pop_B();
+  push_B(a);
+  push_B(b);
+}
+
+/* copy second on stack to top */
+NAMED(_over, "over");
+void over() {
+  int a;
+  int b;
+  a = pop_B();
+  b = pop_B();
+  push_B(b);
+  push_B(a);
+  push_B(b);
+}
+/* add top two items */
+NAMED(_add, "+");
+void add() {
+  int a = pop_B();
+  TOS = a + TOS;
+}
+
+/* bitwise and top two items */
+NAMED(_and, "and");
+void and_() {
+  int a = pop_B();
+  TOS = a & TOS;
+}
+
+/* inclusive or top two items */
+NAMED(_or, "or");
+void or_() {
+  int a = pop_B();
+  TOS = a | TOS;
+}
+
+/* exclusive or top two items */
+NAMED(_xor, "xor");
+void xor_() {
+  int a = pop_B();
+  TOS = a ^ TOS;
+}
+
+/* invert all bits in top of stack */
+NAMED(_invert, "invert");
+void invert() {
+  TOS = ~(TOS);
+}
+
+/* negate top of stack */
+NAMED(_negate, "negate");
+void negate() {
+  TOS = -(TOS);
+}
+
+/* destructively display top of stack, decimal */
+NAMED(_dot, ".");
+void dot() {
+  Serial.print(pop_B());
+  Serial.print(" ");
+}
+
+/* destructively display top of stack, hex */
+NAMED(_dotHEX, ".h");
+void dotHEX() {
+  Serial.print(0xffff & pop_B(), HEX);
+  Serial.print(" ");
+}
+
+/* display whole stack, hex */
+NAMED(_dotShex, ".sh");
+void dotShex() {
+  for (int i = 0; i < STKSIZE_B; i++) dotHEX();
+}
+
+/* display whole stack, decimal */
+NAMED(_dotS, ".s");
+void dotS() {
+  for (int i = 0; i < STKSIZE_B; i++) dot();
+}
+
+/* delay TOS # of milliseconds */
+NAMED(_delay, "delay");
+void del() {
+  delay(pop_B());
+}
+
+/* Toggle pin at TOS and delay(spd), repeat... */
+NAMED(_wiggle, "wiggle");
+void wiggle() {
+  int a = pop_B();
+  pinMode(a, OUTPUT);
+  for (int i = 0; i < 20; i++) {
+    digitalWrite(a, HIGH);
+    delay(100);
+    digitalWrite(a, LOW);
+    delay(100);
+  }
+}
+
+/* TOS is pin number, set it HIGH */
+NAMED(_high, "high");
+void high() {
+  digitalWrite(pop_B(), HIGH);
+}
+
+/* set TOS pin LOW */
+NAMED(_low, "low");
+void low() {
+  digitalWrite(pop_B(), LOW);
+}
+
+/* read TOS pin */
+NAMED(_in, "in");
+void in() {
+  TOS = digitalRead(TOS);
+}
+
+/* make TOS pin an input */
+NAMED(_input, "input");
+void input() {
+  pinMode(pop_B(), INPUT);
+}
+
+/* make TOS pin an output */
+NAMED(_output, "output");
+void output() {
+  pinMode(pop_B(), OUTPUT);
+}
+
+/* make TOS pin an input with weak pullup */
+NAMED(_input_pullup, "input_pullup");
+void input_pullup() {
+  pinMode(pop_B(), INPUT_PULLUP);
+}
+
+/* dump 16 bytes of RAM in hex with ascii on the side */
+void dumpRAM_B() {
+  char buffer[5] = "";
+  char *ram;
+  int p_B = pop_B();
+  ram = (char*)p_B;
+  sprintf(buffer, "%4x", p_B);
+  Serial.print(buffer);
+  Serial.print("   ");
+  for (int i = 0; i < 16; i++) {
+    char c = *ram++;
+    sprintf(buffer, " %2x", (c & 0xff));
+    Serial.print(buffer);
+  }
+  ram = (char*)p_B;
+  Serial.print("   ");
+  for (int i = 0; i < 16; i++) {
+    buffer[0] = *ram++;
+    if (buffer[0] > 0x7f || buffer[0] < ' ') buffer[0] = '.';
+    buffer[1] = '\0';
+    Serial.print(buffer);
+  }
+  push_B(p_B + 16);
+}
+
+/* dump 256 bytes of RAM */
+NAMED(_dumpr, "dump");
+void rdumps_B() {
+  for (int i = 0; i < 16; i++) {
+    Serial.println();
+    dumpRAM_B();
+  }
+}
+
+boolean quit_flag = 0;
+
+NAMED(_quit, "quit");
+void quit() {
+    quit_flag = -1; // true
+}
+
+/* End of Forth interpreter words */
+/* ******************************************** */
+/* Beginning of application words */
+
+
+
+
+/* End of application words */
+/* ******************************************** */
+/* Now build the dictionary */
+
+/* empty words don't cause an error */
+NAMED(_nop, " ");
+void nop() { }
+
+/* Forward declaration required here */
+NAMED(_words, "words");
+void words();
+
+/* table of names and function addresses in flash */
+const entry dictionary[] = {
+  {_nop, nop},
+  {_words, words},
+  {_quit, quit},
+  {_dup, dup},
+  {_drop, drop},
+  {_back, back},
+  {_swap, swap},
+  {_over, over},
+  {_add, add},
+  {_and, and_},
+  {_or, or_},
+  {_xor, xor_},
+  {_invert, invert},
+  {_negate, negate},
+  {_dotS, dotS},
+  {_dotShex, dotShex},
+  {_dot, dot},
+  {_dotHEX, dotHEX},
+  {_delay, del},
+  {_high, high},
+  {_low, low},
+  {_in, in},
+  {_input, input},
+  {_output, output},
+  {_input_pullup, input_pullup},
+  {_wiggle, wiggle},
+  {_dumpr, rdumps_B},
+  {_speed, speed}
+};
+
+/* Number of words in the dictionary */
+const int entries = sizeof dictionary / sizeof dictionary[0];
+
+/* Display all words in dictionary */
+void words() {
+  for (int i = entries - 1; i >= 0; i--) {
+    strcpy(namebuf, dictionary[i].name);
+    Serial.print(namebuf);
+    Serial.print(" ");
+  }
+}
+
+/* Find a word in the dictionary, returning its position */
+int locate() {
+  // Serial.println("DEBUG: locate() was called.");
+  // Serial.print(" Number of entries is: ");
+  // Serial.println(entries);
+  // delay(3000);
+  for (int i = (entries - 1); i >= 0; i--) { // 25 april was not entries -1 just entries
+    // Serial.println("DEBUG inside loop locate");
+    strcpy(namebuf, dictionary[i].name);
+    // if (!strcmp(tib_B, namebuf)) return i;
+    if (!strcmp(tib_B, namebuf)) {
+        Serial.print(" found i: "); Serial.println(i);
+        return i; // just this in production version and no curly braces
+    }
+  }
+  return 0;
+}
+
+/* Is the word in tib_B a number? */
+int isNumber() {
+  char *endptr;
+  strtol(tib_B, &endptr, 0);
+  if (endptr == tib_B) return 0;
+  if (*endptr != '\0') return 0;
+  return 1;
+}
+
+/* Convert number in tib_B */
+int number() {
+  char *endptr;
+  return (int) strtol(tib_B, &endptr, 0);
+}
+
+char ch;
+
+void ok() {
+  if (ch == '\r') Serial.println("ok");
+}
+
+/* Incrementally read command line from serial port */
+byte reading() {
+  if (!Serial.available()) return 1;
+  ch = Serial.read();
+  Serial.write(ch); // KLUDGE 25 April 2021
+  if (ch == '\n') return 0; // terminal pref 25 apr 2021
+  // if (ch == '\r') return 0;
+  // if (ch == '\r') return 1; // not terminal pref 25 apr 2021
+  if (ch == ' ') return 0;
+  if (pos_B < maxtib_B) {
+    tib_B[pos_B++] = ch;
+    tib_B[pos_B] = 0;
+  }
+  return 1;
+}
+
+/* Block on reading the command line from serial port */
+/* then echo each word */
+void readword() {
+  pos_B = 0;
+  tib_B[0] = 0;
+  while (reading());
+  Serial.print('\000'); // Serial.print(tib_B); // KLUDGE 25 April 2021 - was enabled in upstream
+  Serial.print(" ");
+}
+
+/* Run a word via its name */
+void runword() {
+  int place = locate();
+  if (place != 0) {
+    dictionary[place].function();
+    // Serial.println("DEBUG 25 apr: dictionary thing in 'runword' happened already.");
+    ok();
+    return;
+  }
+  if (isNumber()) {
+    push_B(number());
+    ok();
+    return;
+  }
+  Serial.println("?");
+}
+
+/* Arduino main loop */
+
+void blink_three(void) {
+  for (int i=3;i>0;i--) {
+      digitalWrite(13, 1); delay(500); digitalWrite(13, 0); delay(900);
+  }
+}
+
+void setup_second_interpreter(void) {
+  pinMode(13, 1); // Feather RP2040 LED
+  blink_three();
+
+  // Serial.begin(9600);
+  // while (!Serial);
+  // Serial.println ("Forth-like interpreter:");
+  words();
+  Serial.println();
+}
+
+byte run_secondForth(void) {
+  readword();
+  runword();
+  if (quit_flag) {
+      Serial.println(" SEEN: quit_flag = -1");
+      quit_flag = 0; // reset it
+      return 0; // exit secondary loop
+  }
+  return 1;
+}
+
+void secondary_Forth_loop(void) {
+    while(run_secondForth());
+    Serial.println("QUIT detected!");
+    delay(3000);
+    Serial.println();
+    Serial.println("RESTARTing.");
+}
+
+// Sun Apr 25 17:23:54 UTC 2021
+
+
+// extern void setup_second_interpreter(void);
+// extern void secondary_Forth_loop(void);
+
+
+
